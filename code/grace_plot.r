103d908f3ea0d1ff95acc8a24e9e2c48d4cb5955
sjmiller <133066083+sjmiller-sahmri@users.noreply.github.com>
2024-08-05 13:14:36 +0930

    Update grace_plot.r

--- code/grace_plot.r
+++ code/grace_plot.r
@@ -52,8 +52,44 @@ plot_date_of_supply_colour <- plot_date_of_supply + scale_colour_manual(name="An
                                                                          "Macrolide", "Methenamine Hippurate", "Nitrofuran", "Nitroimidazole", "Penicillin", "Tetracycline"))
 plot_date_of_supply_colour
 
-##Fig. 2B + C
-#MICROBIOME
+##Fig. 2C
+all <- read.csv("ARG_dataframe_allppts.csv", header = TRUE)
+
+names(all)[1] <- "Description"  #Re-name first column Description
+
+all_names <- all[,c(1,1163:1178)]
+genes <- all[,1:1162]
+rownames(genes) <- genes[,1]
+genes <- genes[,2:1162]
+sqrt_all <- sqrt(genes)
+
+df_bray_all <- vegdist(sqrt_all, "bray")
+
+pcoa_all <- pcoa(df_bray_all)
+
+pcoa_variance_all <- pcoa_all$values
+
+pcoa_all_coordinates <- data.frame(all_names,pcoa_all[["vectors"]])
+
+gg_genes_pcoa_all <- merge(pcoa_all_coordinates,aggregate(cbind(mean.X=Axis.1,mean.Y=Axis.2)~any_tetracycline_used,pcoa_all_coordinates,mean),by="any_tetracycline_used")
+
+pcoa_genes_all<- ggplot(data=gg_genes_pcoa_all, mapping=aes(Axis.1,Axis.2,color=any_tetracycline_used, fill = any_tetracycline_used))+ 
+  theme_linedraw()+ 
+  theme(panel.grid = element_blank(), 
+        axis.text = element_blank(), 
+        axis.title = element_text(size=11),
+        legend.background = element_blank(),
+        legend.title = element_blank(),
+        legend.text=element_text(size=8))+
+  stat_ellipse(aes(Axis.1,Axis.2, color=any_tetracycline_used), geom = "polygon", alpha = 1/8, type = "t", level = 0.80)+
+  theme(legend.spacing.y = unit(0.15, "cm"), legend.position = c(0.17,0.85))+
+  #scale_fill_discrete(breaks=c('UTI','No UTI','Non-elderly'))+
+  geom_point(size=1)+
+  xlab("PCO1 (19.7% of total variation)") + ylab("PCO2 (5.9% of total variation)")+
+  geom_segment(aes(mean.X, mean.Y, xend=Axis.1, yend=Axis.2))
+pcoa_genes_all
+
+##Fig. 2D
 all <- read.csv("Taxa_dataframe_allppts.csv", header = TRUE)
 
 names(all)[1] <- "Description"  #Re-name first column Description
@@ -90,40 +126,3 @@ pcoa_taxa_all<- ggplot(data=gg_taxa_pcoa_all, mapping=aes(Axis.1,Axis.2,color=an
   geom_segment(aes(mean.X, mean.Y, xend=Axis.1, yend=Axis.2))
   #coord_fixed()
 pcoa_taxa_all
-
-##RESISTOME
-all <- read.csv("ARG_dataframe_allppts.csv", header = TRUE)
-
-names(all)[1] <- "Description"  #Re-name first column Description
-
-all_names <- all[,c(1,1163:1178)]
-genes <- all[,1:1162]
-rownames(genes) <- genes[,1]
-genes <- genes[,2:1162]
-sqrt_all <- sqrt(genes)
-
-df_bray_all <- vegdist(sqrt_all, "bray")
-
-pcoa_all <- pcoa(df_bray_all)
-
-pcoa_variance_all <- pcoa_all$values
-
-pcoa_all_coordinates <- data.frame(all_names,pcoa_all[["vectors"]])
-
-gg_genes_pcoa_all <- merge(pcoa_all_coordinates,aggregate(cbind(mean.X=Axis.1,mean.Y=Axis.2)~any_tetracycline_used,pcoa_all_coordinates,mean),by="any_tetracycline_used")
-
-pcoa_genes_all<- ggplot(data=gg_genes_pcoa_all, mapping=aes(Axis.1,Axis.2,color=any_tetracycline_used, fill = any_tetracycline_used))+ 
-  theme_linedraw()+ 
-  theme(panel.grid = element_blank(), 
-        axis.text = element_blank(), 
-        axis.title = element_text(size=11),
-        legend.background = element_blank(),
-        legend.title = element_blank(),
-        legend.text=element_text(size=8))+
-  stat_ellipse(aes(Axis.1,Axis.2, color=any_tetracycline_used), geom = "polygon", alpha = 1/8, type = "t", level = 0.80)+
-  theme(legend.spacing.y = unit(0.15, "cm"), legend.position = c(0.17,0.85))+
-  #scale_fill_discrete(breaks=c('UTI','No UTI','Non-elderly'))+
-  geom_point(size=1)+
-  xlab("PCO1 (19.7% of total variation)") + ylab("PCO2 (5.9% of total variation)")+
-  geom_segment(aes(mean.X, mean.Y, xend=Axis.1, yend=Axis.2))
-pcoa_genes_all
